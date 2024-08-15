b42a5edd0202ad2bc02cdaacacb09d6f28f508a1
Lucas Fogaça <lucas.fogaca@vsoft.com.br>
2024-08-14 21:20:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int x, y;
+double divisao;
+Console.WriteLine("Digitar o valor de X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o valor de y");
+y = Convert.ToInt32(Console.ReadLine());
+
+divisao = (double) x / y;
+
+Console.WriteLine($"O valor da divisao entre {x} e {y} é igual a: {divisao}");
+
+
